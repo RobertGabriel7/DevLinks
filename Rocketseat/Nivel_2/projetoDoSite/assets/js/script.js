ecb62261cd186989408baf4ca9efe15570402120
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-05-06 11:24:48 -0300

    Projeto DevLinks

---
+++ Rocketseat/Nivel_2/projetoDoSite/assets/js/script.js
@@ -0,0 +1,34 @@
+function teste(){
+    /* documentElement é o documento HTML e a tag html */
+    var html = document.documentElement
+    
+    var img = document.querySelector("#profile img");
+
+    /* alert("teste") */
+    
+    /* html.classList(é um objeto que tem functions).contains('atributo')( que é uma function de classList) */
+    /* html. a lista de classe (classList). contem (contains) ('a classe no html') */
+    if(html.classList.contains('light')){
+        /* Nesse caso aqui, objeto classList tem a function de remover uma class existente no document html */
+        html.classList.remove("light")
+        html.style.transition = "all 2s";
+        
+        /* Para atribuir um atributo para uma img: setAttribute('dizer qual atribuito vou utilizar', 'o valor do atributo') */
+        img.setAttribute('src', '../img/carinha_do_ti-noite.jpg');
+    }else{
+        html.classList.add("light");
+        img.setAttribute('src', '../img/carinha_do_ti.jpg');
+        img.setAttribute('alt', 'Foto pessoal para o tema claro do site')
+    }
+    
+    /* 
+    
+    Com function toggle(), eu posso substituir código acima por esse código abaixo: 
+    
+    a function toggle() significa alterar, ela alterar um class se tiver ou não. Por exemplo, se existe a class "light", function toggle() remove a class, mas se não existir, a function toggle() cria a class com esse nome.
+    
+    html.classList.toggle("light")
+
+    */
+}
+
